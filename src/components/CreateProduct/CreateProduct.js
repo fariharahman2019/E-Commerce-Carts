ca6dfcc1df271365990e061c0dfe89da61c1fe0e
fariharahman2019 <fariha.rahman1@g.bracu.ac.bd>
2023-09-12 23:28:35 +0600

    CreateProduct Complete

--- src/components/CreateProduct/CreateProduct.js
+++ src/components/CreateProduct/CreateProduct.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateProduct() {
+    const [product, setProduct] = useState({
+      title: '',
+      category: '',
+      description: '',
+    });
+  
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setProduct({
+        ...product,
+        [name]: value,
+      });
+    };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 shadow-md rounded-lg w-96">
@@ -29,16 +43,18 @@ function CreateProduct() {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="productName" className="block text-sm font-medium text-gray-700">
-              Select Description
-            </label>
-            <input
-              type="textarea"
-              id="productName"
-              name="productName"
-              className="mt-1 p-2 block w-full rounded-md border border-gray-900 focus:border-indigo-600 focus:ring focus:ring-indigo-200"
-            />
-          </div>
+          <label className="block text-gray-600 font-semibold mb-1" htmlFor="description">
+            Select Description
+          </label>
+          <textarea
+            name="description"
+            id="description"
+            rows="4"
+            className="w-full p-2 border rounded-md focus:outline-none border-gray-900 focus:border-blue-400 h-48"
+            value={product.description}
+            onChange={handleInputChange}
+          />
+        </div>
           <div className="mb-4">
             <label htmlFor="productName" className="block text-sm font-medium text-gray-700">
               Select Price
@@ -51,7 +67,29 @@ function CreateProduct() {
               className="mt-1 p-2 block w-full rounded-md border border-gray-900 focus:border-indigo-600 focus:ring focus:ring-indigo-200"
             />
           </div>
-          <div className="text-center">
+
+          <div class="grid grid-cols-2 gap-4">
+        <div>
+            <label for="input1" class="block text-gray-600 font-semibold mb-1">Price</label>
+            <input type="text" id="input1" class="w-full p-2 border rounded-md focus:outline-none focus:border-blue-400" />
+        </div>
+        <div className='mt-7'>
+        <select
+            name="category"
+            id="category"
+            className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-400"
+            value={product.category}
+            onChange={handleInputChange}
+          >
+            <option value="">Select Option</option>
+            <option value="category1">Per Hour</option>
+            <option value="category2">Per Day</option>
+            <option value="category3">Per Month</option>
+          </select>
+        </div>
+        </div>
+
+          <div className="text-center m-5">
             <button
               type="submit"
               className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
