1a4989e0bbe9d033ecb63baedcdce80a3bdf7b27
fariharahman2019 <fariha.rahman1@g.bracu.ac.bd>
2023-09-14 04:10:18 +0600

    Login and Reg added on app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './common/Navbar';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import PrivateRoute from './components/Privateroute/PrivateRoute';
+
 import './App.css';
 
 function App() {
@@ -9,6 +13,9 @@ function App() {
       <BrowserRouter>
       <Navbar></Navbar>
         <Routes >
+          <Route path="/user/registration" element={<Register></Register>}/>
+          <Route path="/user/login" element={<Login></Login>}/>
+          
         </Routes>
       </BrowserRouter>
     </div>
