1206af81434e0b86a169f16b1d5b1204cb0a18fc
fariharahman2019 <fariha.rahman1@g.bracu.ac.bd>
2023-09-14 03:53:55 +0600

    dd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-import { Routes, Route} from 'react-router-dom';
-import SignIn from './components/SignIn/SignIn';
-import SignUp from './components/SignUp/SignUp';
-import CreateProduct from './components/CreateProduct/CreateProduct';
-import EditProduct from './components/EditProduct/EditProduct';
-import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-          <Route path="/" element={<Home></Home>} />
-          <Route path="/home" element={<Home></Home>} />
-          <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/CreateProduct" element={<CreateProduct />} />
-          <Route path="/EditProduct" element={<EditProduct />} />
-         
-      </Routes>      
+     
     </div>
   );
 }
